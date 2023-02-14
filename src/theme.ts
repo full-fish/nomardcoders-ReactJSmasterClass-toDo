db55b3b98876151e402b5a9d17cb0ca8dd124671
manseon <91870110+full-fish@users.noreply.github.com>
2023-02-14 21:44:07 +0900

    feat

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components'
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'whitesmoke',
+  textColor: 'black',
+  accentColor: '#9c88ff',
+  cardBgColor: 'white',
+}
+export const darkTheme: DefaultTheme = {
+  bgColor: '#353b48',
+  textColor: 'white',
+  accentColor: '#e84118',
+  cardBgColor: 'transparent',
+}
+//test
